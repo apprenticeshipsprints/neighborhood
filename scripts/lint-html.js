c067375e6df3afc2e22d6f8b140456a602056840
Brian Peiris <brianpeiris@gmail.com>
2018-04-22 18:58:02 -0700

    fix linter script

--- scripts/lint-html.js
+++ scripts/lint-html.js
@@ -8,26 +8,20 @@ const os = require("os");
 const shell = require("shelljs");
 
 (async function() {
-  function lintFile(tempDir, arg, file) {
+  function lintFile(tempDir, args, file) {
     const out = path.join(tempDir, file);
     shell.mkdir("-p", path.dirname(out));
     shell.sed(/<%.+%>/, "", file).to(out);
-    const result = shell.exec(`node_modules/.bin/htmlhint ${arg} --config=.htmlhintrc ${out}`);
+    const result = shell.exec(`node_modules/.bin/htmlhint ${args} --config=.htmlhintrc ${out}`);
     return result.code;
   }
 
   let result = 0;
   if (process.argv.length > 2) {
     const tempDir = await mkdtemp(path.join(os.tmpdir(), "lint-html-"));
-    let files;
-    let arg = "";
-    if (process.argv.length === 4) {
-      arg = process.argv[2];
-      files = process.argv[3];
-    } else {
-      files = process.argv[2];
-    }
-    const results = shell.ls(files).map(lintFile.bind(null, tempDir, arg));
+    const htmlHintArgs = process.argv.slice(2, -1).join(" ");
+    const files = process.argv[process.argv.length - 1];
+    const results = shell.ls(files).map(lintFile.bind(null, tempDir, htmlHintArgs));
     result = results.reduce((a, r) => a + r, 0);
     shell.rm("-r", tempDir);
   }
